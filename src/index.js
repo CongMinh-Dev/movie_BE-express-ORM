ac24c3d2aceff0886c10561d04c9177e55fb5068
Minh <lightstormlxag93@gmail.com>
2026-06-25 16:35:08 +0700

    cicd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import rootRouter from './routes/rootRouter.js'
-
+console.log("Check Env:", process.env.DB_DIALECT);
 const app = express()
 
 app.use(cors())
